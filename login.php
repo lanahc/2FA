346fb7be937cd4f09a899a3933aeb3fd4657ef4f
lanahc <filanahchoi17@gmail.com>
2025-01-16 17:17:32 +0300

    changes

--- login.php
+++ login.php
@@ -33,7 +33,7 @@
             </div>
 
             <div>
-              <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+              <p class="mb-0">Don't have an account? <a href="signup.php" class="text-white-50 fw-bold">Sign Up</a>
               </p>
             </div>
 
