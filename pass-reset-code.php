a4b19e19e0704f32f401944b9d385f17c7043ac8
lanahc <filanahchoi17@gmail.com>
2025-01-16 22:06:36 +0300

    changes

--- pass-reset-code.php
+++ pass-reset-code.php
@@ -4,7 +4,7 @@ include('dbconn.php');
 
 if(isset($POST['password_reset_link']))
 {
-    $email =mysqli_real_escape_string($con, $_POST[])
+    $email =mysqli_real_escape_string($con, $_POST['email']);
 }
 
 ?>
