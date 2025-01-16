8b1c80ade6e51caf901b39cba3abdf6ddba9b591
lanahc <filanahchoi17@gmail.com>
2025-01-16 22:14:34 +0300

    changes

--- pass-reset-code.php
+++ pass-reset-code.php
@@ -6,6 +6,22 @@ if(isset($POST['password_reset_link']))
 {
     $email =mysqli_real_escape_string($con, $_POST['email']);
     $token = md5(rand());
+
+    $check_email = "SELECT email * FROM users WHERE email = '$email' LIMIT 1 ";
+    $check_email_run = mysqli_query($con,$check_email);
+
+    if(mysqli_num_rows($check_email_run) >0)
+    {
+      $row = mysqli_fetch_array($check_email_run);
+      $get_name = $row['name'];
+      $get_email = $row['email'];
+    }
+    else
+    {
+        $_SESSION['status'] = "No Email Found";
+        header("Location:passreset.php");
+        exit(0);
+    }
 }
 
 ?>
