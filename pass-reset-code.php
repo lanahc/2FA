fe18035ac4d4245b336b20f7beef07f974338cac
lanahc <filanahchoi17@gmail.com>
2025-01-20 11:36:34 +0300

    changes

--- pass-reset-code.php
+++ pass-reset-code.php
@@ -21,7 +21,7 @@ try {
     $mail->Username   = 'user@example.com';                     //SMTP username
     $mail->Password   = '***';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('from@example.com', $get_name);
