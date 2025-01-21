1d3e727eb9cd114782d2bca6a97393445a5f55b5
lanahc <filanahchoi17@gmail.com>
2025-01-21 11:47:33 +0300

    changes

--- pass-reset-code.php
+++ pass-reset-code.php
@@ -53,7 +53,7 @@ try {
 
 }
 
-if(isset($POST['password_reset_link']))
+if(isset($_POST['password_reset_link']))
 {
     $email =mysqli_real_escape_string($con, $_POST['email']);
     $token = md5(rand());
